d4e657bc1ccdbbd1ec36e992515d42fee14c68e3
oisraa <eo2394022@gmail.com>
2024-11-28 22:29:09 +0300

    work

--- server.ts
+++ server.ts
@@ -6,28 +6,23 @@ const app=express();
 import {globalError} from "./middlewares/errorMiddleware"
 import {connectDB} from "./DB/connectDb"
 import path from "path"
-<<<<<<< HEAD
+
 import teamRouter from './routes/teamRoutes'; // Import your routes
 import historyRoutes from "./routes/historyRoutes";
 import awardRoutes from "./routes/awardRoutes"; 
 import missionVisionRoutes from "./routes/missionVisionRoutes"; 
-import  {specialFeaturesRouter} from "./routes/specialFeature.route"
-import {subjectRouter} from "./routes/subject.route"
-import {roomRouter} from "./routes/room.route"
+
 
 app.use(express.json());
 
 
-app.use("/api/specialfeatures",specialFeaturesRouter)
-app.use("/api/subjects",subjectRouter)
-app.use("/api/rooms",roomRouter)
 app.use('/team-members', teamRouter); // All routes in teamRouter will now be prefixed with /team-members
 app.use("/api/history", historyRoutes);
 app.use("/api/awards", awardRoutes);
 app.use("/api/mission-vision", missionVisionRoutes);
 
 app.use('/uploads/member', express.static(path.join(__dirname, 'uploads/member')));
-=======
+
 
 import {commonQuestionRouter} from "./routes/CommonQuestionsRoutes"
 import {TestimonialsRouter} from "./routes/TestimonialsRoutes"
@@ -41,7 +36,7 @@ app.use("/api/features",FeaturesRouter)
 
 
 app.use(express.static(path.join(__dirname,"../uploads")))
->>>>>>> 034a0b5f0ca3250e3789cf6cbd8e844c03ac01a2
+
 
 app.use(globalError)
 
