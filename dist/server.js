8514ae580be45169ccf8ec1affa413fdcfe45af2
AbdAlrahman13 <blalabo89@gmail.com>
2024-12-29 09:30:47 +0300

    commit

--- dist/server.js
+++ dist/server.js
@@ -44,8 +44,8 @@ app.use("/api/activities", activities_1.activitiesRouter);
 app.use("/api/events", events_1.eventsRouter);
 app.use("/api/message", messagesRoutes_1.default);
 app.use('/uploads/member', express_1.default.static(path_1.default.join(__dirname, 'uploads/member')));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../uploads")));
 app.use('/api/general', ServicesRoutes_1.default);
-app.use(express_1.default.static(path_1.default.join(__dirname, "/uploads")));
 app.use(errorMiddleware_1.globalError);
 app.listen(process.env.PORT, () => {
     console.log(`listening on port:${process.env.PORT}....`);
